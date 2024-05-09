69b8a97be73cb0627959e22c8c76acf481999914
Harshal Vaidya <harshal.vaidya300@gmail.com>
2024-05-09 10:58:42 -0400

    Created customer facing part for products and product card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,67 +1,99 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import db from "@/db/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
 
 async function getSalesData() {
-  const data = await db.order.aggregate({_sum: { pricePaidInCents: true}, _count: true})
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
 
   return {
     amount: (data._sum.pricePaidInCents || 0) / 100,
-    numberOfSales: data._count 
-  }
+    numberOfSales: data._count,
+  };
 }
 
 async function getUserData() {
   const [userCount, orderData] = await Promise.all([
-    db.user.count(), 
+    db.user.count(),
     db.order.aggregate({
-      _sum: {pricePaidInCents: true}
-    })
-  ])
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
 
   return {
     userCount,
-    averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount / 100
-  }
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
 }
 
 async function getProductData() {
   const [activeCount, inActiveCount] = await Promise.all([
-    db.product.count({where: {isAvailableForPurchase: true}}),
-    db.product.count({where: {isAvailableForPurchase: false}}) 
-  ])
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
 
-  return {activeCount, inActiveCount}
+  return { activeCount, inActiveCount };
 }
 
 export default async function AdminDashboard() {
   const [salesData, userData, productData] = await Promise.all([
     getSalesData(),
     getUserData(),
-    getProductData()
-  ]) 
-  
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} body={formatCurrency(salesData.amount)} />
-    <DashboardCard title="Customers" subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`} body={formatNumber(userData.userCount)} />
-    <DashboardCard title="Active Products" subtitle={`${formatNumber(productData.inActiveCount)} Inactive Products`} body={formatNumber(productData.activeCount)} />
-  </div>
+    getProductData(),
+  ]);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(
+          productData.inActiveCount
+        )} Inactive Products`}
+        body={formatNumber(productData.activeCount)}
+      />
+    </div>
+  );
 }
 
 type DashboardCardProps = {
-  title: string
-  subtitle: string
-  body: string
-}
+  title: string;
+  subtitle: string;
+  body: string;
+};
 
-function DashboardCard({title, subtitle, body}: DashboardCardProps) {
-  return <Card>
-    <CardHeader>
-      <CardTitle>{title}</CardTitle>
-      <CardDescription>{subtitle}</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <p>{body}</p>
-    </CardContent>
-  </Card>
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
